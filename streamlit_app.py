577fe161271a9bc3729f7df01f2cec5687598cfe
Abraham Setiawan <52783886+abrahamzetz@users.noreply.github.com>
2023-10-20 21:21:02 +0200

    Update streamlit_app.py
    removed on_change

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,4 +43,4 @@ my_data_rows = my_cur.fetchall()
 st.header("The fruit load list contains:")
 st.dataframe(my_data_rows)
 
-add_my_fruit = st.text_input('What fruit would you like to add?', on_change='Thanks for adding ')
+add_my_fruit = st.text_input('What fruit would you like to add?')
