3587d3df7146e14d369c903b8df0e479816617cc
Abraham Setiawan <52783886+abrahamzetz@users.noreply.github.com>
2023-10-20 20:00:12 +0200

    Update streamlit_app.py
    added fruit text input

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,10 +23,12 @@ st.dataframe(fruits_to_show)
 
 #New Section to display fruityvice api response
 st.header ('Fruityvice Fruit Advice!')
+fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
+st.write('The user entered ', fruit_choice)
 
 # New section to display Fruitvice API response
 import requests
-fruityvice_response = requests.get('https://fruityvice.com/api/fruit/' + 'kiwi')
+fruityvice_response = requests.get('https://fruityvice.com/api/fruit/' + fruit_choice)
 
 # take the json version of the response and normalize it
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json ())
